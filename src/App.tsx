60d2e75833f9de7ea9b42cc9cc8d8acdfb362633
ryan <lrcrews@gmail.com>
2021-05-05 14:51:48 -0700

    hook up to google docs spreadsheet for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import * as Tabletop from "tabletop";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // Needed to copy the actual spreadsheet so I could publish to web, so this
+  // stuff isn't "live" yet.  Maybe exnor/tex adam can publish the main sheet.
+  //
+  // https://docs.google.com/spreadsheets/d/1GL02V9TNPYwf7gcRyMVkpH36GzxuGBHD24KE-6KXuoo/edit?usp=sharing
+
+  useEffect(() => {
+    Tabletop.init({
+      key: "1GL02V9TNPYwf7gcRyMVkpH36GzxuGBHD24KE-6KXuoo",
+    })
+      .then((data: { [key: string]: any }) => {
+        console.log(
+          `data: ${Object.keys(data["Sorted by Name"]["prettyColumns"])}`
+        );
+        //setData(data);
+      })
+      .catch((err: string) => console.warn(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">

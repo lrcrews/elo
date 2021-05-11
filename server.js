fc8d40f7ae8d1ec11aa3f309956e8cc014272580
ryan <lrcrews@gmail.com>
2021-05-10 18:10:56 -0700

    smells like devops

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/build"));
+
+// Redirect direct URLs back to index so React may then route them properly
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000);

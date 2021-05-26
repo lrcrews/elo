c3232a5d015eba6cfe18e63b92c5901cf734a21e
ryan <lrcrews@gmail.com>
2021-05-25 22:28:02 -0700

    data entry tuesday may 25

--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1621919122783;
+const MANUALLY_UPDATED_DATE = 1622006790029;
 
 export default function SiteFooter() {
   return (

6f5cd47dbe97f03a72f3a008aa9306b11401a60f
ryan <lrcrews@gmail.com>
2021-05-31 21:36:06 -0700

    data entry for wars on mon may 31

--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1622328520356;
+const MANUALLY_UPDATED_DATE = 1622522078366;
 
 export default function SiteFooter() {
   return (

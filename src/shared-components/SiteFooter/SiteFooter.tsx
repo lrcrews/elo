6e88833c0d10c3c75ef63a84ef45b320851176cd
ryan <lrcrews@gmail.com>
2021-10-21 14:49:19 -0700

    data oct 20

--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH, BREAKDOWN_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1634766056983;
+const MANUALLY_UPDATED_DATE = 1634852742157;
 
 export default function SiteFooter() {
   return (

761e3799d6ab0cfdb490d38ddc8650e6ad4bd75e
ryan <lrcrews@gmail.com>
2021-12-14 16:51:17 -0800

    data dec 13

--- src/data/EloFilePaths.ts
+++ src/data/EloFilePaths.ts
@@ -43,6 +43,7 @@
 const PREFIX = `${process.env.PUBLIC_URL}/data/ELO-`;
 
 export const ELO_FILE_PATHS = [
+  `${PREFIX}2021-12-13.csv`,
   `${PREFIX}2021-12-10.csv`,
   `${PREFIX}2021-12-08-and-09.csv`,
   `${PREFIX}2021-12-08-and-09.csv`,
--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH, BREAKDOWN_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1639363729046;
+const MANUALLY_UPDATED_DATE = 1639529112501;
 
 export default function SiteFooter() {
   return (

d46fe633488165c4674b3aa77dd990131ddd7021
ryan <lrcrews@gmail.com>
2022-02-27 11:17:35 -0800

    data feb 23, 24, and 25 (one file)

--- src/data/EloFilePaths.ts
+++ src/data/EloFilePaths.ts
@@ -43,6 +43,9 @@
 const PREFIX = `${process.env.PUBLIC_URL}/data/ELO-`;
 
 export const ELO_FILE_PATHS = [
+  `${PREFIX}2022-02-23-and-24-and-25.csv`,
+  `${PREFIX}2022-02-23-and-24-and-25.csv`,
+  `${PREFIX}2022-02-23-and-24-and-25.csv`,
   `${PREFIX}2022-02-22.csv`,
   `${PREFIX}2022-02-21.csv`,
   `${PREFIX}2022-02-18.csv`,
--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH, BREAKDOWN_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1645761166124;
+const MANUALLY_UPDATED_DATE = 1645988986706;
 
 export default function SiteFooter() {
   return (

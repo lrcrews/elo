e6f37bdccb8f28c0986e2066fe583d061ea974b1
ryan <lrcrews@gmail.com>
2022-03-23 11:38:21 -0700

    data mar 18 and 21 (one file)

--- src/data/EloFilePaths.ts
+++ src/data/EloFilePaths.ts
@@ -43,6 +43,7 @@
 const PREFIX = `${process.env.PUBLIC_URL}/data/ELO-`;
 
 export const ELO_FILE_PATHS = [
+  `${PREFIX}2022-03-18-and-21.csv`,
   `${PREFIX}2022-03-17.csv`,
   `${PREFIX}2022-03-16.csv`,
   `${PREFIX}2022-03-15.csv`,

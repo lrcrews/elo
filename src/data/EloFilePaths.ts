c74d4e933e20f7b51b7b2c4c73ae68e39adc4142
ryan <lrcrews@gmail.com>
2022-07-30 15:56:38 -0700

    data week end jul 22

--- src/data/EloFilePaths.ts
+++ src/data/EloFilePaths.ts
@@ -43,6 +43,7 @@
 const PREFIX = `${process.env.PUBLIC_URL}/data/ELO-WEEK-END-`;
 
 export const ELO_FILE_PATHS = [
+  `${PREFIX}2022-07-22.csv`,
   `${PREFIX}2022-07-15.csv`,
   `${PREFIX}2022-07-08.csv`,
   `${PREFIX}2022-07-01.csv`,

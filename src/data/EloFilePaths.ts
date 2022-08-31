25a24ec9551c2d5b300018b28b111302b17487ff
ryan <lrcrews@gmail.com>
2022-08-31 10:16:21 -0700

    data week end aug 26

--- src/data/EloFilePaths.ts
+++ src/data/EloFilePaths.ts
@@ -43,6 +43,7 @@
 const PREFIX = `${process.env.PUBLIC_URL}/data/ELO-WEEK-END-`;
 
 export const ELO_FILE_PATHS = [
+  `${PREFIX}2022-08-26.csv`,
   `${PREFIX}2022-08-19.csv`,
   `${PREFIX}2022-08-12.csv`,
   `${PREFIX}2022-08-05.csv`,
--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1661211861960;
+const MANUALLY_UPDATED_DATE = 1661965835196;
 
 export default function SiteFooter() {
   return (

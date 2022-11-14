db3b2b201ad8c80fa499db1fc25191d8bb209539
ryan <lrcrews@gmail.com>
2022-11-13 21:45:25 -0800

    data week end nov 11

--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1667782617818;
+const MANUALLY_UPDATED_DATE = 1668404359322;
 
 export default function SiteFooter() {
   return (

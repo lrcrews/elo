e5e4f53b4bc637d5154152d2c5fe6333f87778f1
ryan <lrcrews@gmail.com>
2023-08-02 17:13:08 -0700

    wtf heroku

--- src/shared-components/SiteFooter/SiteFooter.tsx
+++ src/shared-components/SiteFooter/SiteFooter.tsx
@@ -4,7 +4,7 @@ import { ABOUT_PATH } from "../../routes/routes";
 
 import "./SiteFooter.scss";
 
-const MANUALLY_UPDATED_DATE = 1691020805851;
+const MANUALLY_UPDATED_DATE = 1691021554975;
 
 export default function SiteFooter() {
   return (
